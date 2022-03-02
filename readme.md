0f1c87194c750a2d3e96c9dcf752f7eb0af41bcf
Aurélien <aurelien.strimelle@bstorm.be>
2022-03-02 16:06:49 +0100

    Add templating and public file

--- readme.md
+++ readme.md
@@ -1,5 +1,14 @@
 # Demo 04 - Web Server - La suite
 
-- Routing
-- Templating
-- Formulaire
+## Routing
+Mise en place simple avec des conditions et une regex
+
+## Templating
+Liste de moteur de template : 
+ - Pug      : https://pugjs.org/
+ - EJS      : https://ejs.co/
+ - Jade     : https://jade-lang.com/
+ - Mustache : https://github.com/janl/mustache.js/
+Choix pour la demo → EJS
+
+## Formulaire
--- server.js
+++ server.js
@@ -1,37 +1,82 @@
 const http = require('http');
-const url = require('url');
+const path = require('path');
+const fs = require('fs');
 const { URL, URLSearchParams } = require('url');
+const ejs = require('ejs');
 
-const sendServerResponse = (res, data, code = 200) => {
-    res.writeHead(code, { "Content-Type": "text/plain" });
-    res.write(data);
-    res.end();
+const sendServerResponse = (res, pageName, data = {}, code = 200) => {
+    // Récuperation du fichier "view" de la page demandé
+    const file = path.resolve(__dirname, 'views', 'pages', pageName + '.ejs');
+
+    // Utilisation du moteur de template 'EJS'    
+    ejs.renderFile(file, data, function (error, pageRender) {
+        if (error) {
+            console.log(error);
+            res.writeHead(500);
+            res.end();
+            return;
+        }
+
+        // Réponse du server
+        res.writeHead(code, { "Content-Type": "text/html" });
+        res.write(pageRender);
+        res.end();
+    });
+};
+
+const publicFileExists = (targetFile) => {
+    const filename = path.resolve(__dirname, 'public' + targetFile);
+
+    if (fs.existsSync(filename)) {
+        return fs.statSync(filename).isFile();
+    }
+    return false;
+};
+
+const sendFile = (res, targetFile) => {
+    const filename = path.resolve(__dirname, 'public' + targetFile);
+
+    fs.readFile(filename, (error, data) => {
+        res.writeHead(200);
+        res.end(data, 'utf-8');
+    });
 };
 
 // Création du server
 const server = http.createServer((req, res) => {
-
     // Extraction des informations de l'url
     const url = new URL(req.url, 'http://' + req.headers.host);
     console.log(url.pathname);
 
     // Systeme de routage (Simple !!!)
+    // - Fichier public!
+    if (publicFileExists(url.pathname)) {
+        sendFile(res, url.pathname);
+    }
     // - Page d'accueil (→ Avec une Regex)
-    if (/^\/(home\/?)?$/i.test(url.pathname)) {
-        sendServerResponse(res, 'Home !');
+    else if (/^\/(home\/?)?$/i.test(url.pathname)) {
+        const now = new Date();
+        const today = now.toLocaleDateString('fr-be');
+        sendServerResponse(res, 'home/index', { today });
     }
     // - About
     else if (url.pathname.toLowerCase() === '/about') {
-        sendServerResponse(res, 'About !');
+        sendServerResponse(res, 'home/about');
     }
     // - Contact
     else if (url.pathname.toLowerCase() === '/contact') {
-        sendServerResponse(res, 'Formulaire de contact');
+        console.log(req.method);
+        if (req.method === 'GET') {
+            sendServerResponse(res, 'contact/index');
+        }
+        else {
+
+            sendServerResponse(res, 'contact/response');
+        }
     }
     else {
-        sendServerResponse(res, 'Page not found !', 404);
+        sendServerResponse(res, 'error/404', 404);
     }
-
 });
 
 
